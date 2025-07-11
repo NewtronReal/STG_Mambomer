4b8015d897c17ad1d36544b7cd2f04b087585b87
NewtronReal <ronstephenmathew@gmail.com>
2025-07-11 18:48:39 +0530

    auto device

--- something.py
+++ something.py
@@ -1,6 +1,6 @@
 from Transformer.modules.MAE_Mamba import MAE_Mamba
 from Transformer.modules.GraphFeatures import GraphFeatures
-from Transformer.data.data import PrepareDataset
+from Transformer.data.data import PrepareDataset,get_device
 import torch
 import pandas as pd
 
@@ -13,11 +13,11 @@ one_batch = next(iter(traind))[0]
 B,W,m,N,D = one_batch.shape
 compound = one_batch.view(B*W,m,N,D)
 
-graph = GraphFeatures(adj_path = "Transformer/adj.npz",device='mps')
+graph = GraphFeatures(adj_path = "Transformer/adj.npz",device=get_device())
 # graph.adj = torch.ones(306,306).long()
 # graph.spd = torch.ones(306,306)
 # graph.in_degree = torch.ones(306).long()
 # graph.out_degree = torch.ones(306).long()
-g = MAE_Mamba(graph=graph,N=307,T=3,hno=8).to("mps")
+g = MAE_Mamba(graph=graph,N=307,T=3,hno=8).to(get_device())
 a = g(compound,.5)
 print(a[2])
